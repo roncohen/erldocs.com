904a0599bb4f08df66d055a6cc0ca05cc59e34b7
Dale <dale@dale.(none)>
2009-08-23 22:51:59 +0100

    added module index, syntax highlighting, side bar, and ugly code

--- src/erldocs.erl
+++ src/erldocs.erl
@@ -1,14 +1,17 @@
 -module(erldocs).
 
--export([ all/1 ]).
+-export([ all/1, module_index/1 ]).
 
 all(Otp) ->
     
     ok = build(Otp),
-    ok = index(Otp),
-
+    ok = javascript_index(Otp),
+    ok = module_index(Otp),
+    
     % Generate index page
-    Html    = erlref_wrap("index", index_tpl(), ""),
+    [Name | _Rest] = lists:reverse(string:tokens(Otp, "_")),
+    
+    Html    = erlref_wrap("Home", index_tpl(Name, ""), ""),
     File    = filename:join([root(), "www", Otp, "index.html"]),
     HtmlStr = xml_to_str(Html, "<!DOCTYPE html>"),
     ok = file:write_file(File, HtmlStr).
@@ -31,7 +34,27 @@ build(Otp, DocSrc) ->
             render(Type, root(), Otp, App, Mod, Content)
     end.
 
-index(Otp) ->
+module_index(Otp) ->
+    Index = index(Otp, find_docs(Otp), [], long),
+    Mods  = lists:filter(fun(["mod"|_]) -> true; (_) -> false end, Index),
+    SMods = lists:reverse(lists:sort(Mods)),
+    Xml = [{h1, [], ["Module Index"]},
+           {hr, [], []}, {br, [], []},
+           {'div', [], module_index(SMods, [])}],
+
+    Html  = erlref_wrap("Module Index", Xml, ""),
+    HtmlStr = xml_to_str(Html, "<!DOCTYPE html>"),
+    File = filename:join([root(), "www", Otp, "module_index.html"]),
+    ok   = file:write_file(File, HtmlStr).
+    
+module_index([], Acc) ->
+    Acc;
+module_index([["mod", App, Mod, Summary]|Tail], Acc) ->
+    Html = {p, [], [{a, [{href, App++"/"++Mod++".html"}], [Mod]},
+                    {br, [], []}, Summary]},
+    module_index(Tail, [Html | Acc]).
+
+javascript_index(Otp) ->
     
     Sort = fun(["app" | _Rest1], ["mod" | _Rest2]) -> true;
               (["app" | _Rest1], ["fun" | _Rest2]) -> true;
@@ -39,26 +62,24 @@ index(Otp) ->
               (_, _) -> false
            end,
 
-    Index = index(Otp, find_docs(Otp), []),
+    Index = index(Otp, find_docs(Otp), [], short),
     % Heh, cheating, will probably break
     Js = format("var index = ~p;",[lists:sort(Sort, Index)]),
     
     File = filename:join([root(), "www", Otp, "erldocs_index.js"]),
     ok   = file:write_file(File, Js).
 
-index(_Otp, [], Acc) ->
+index(_Otp, [], Acc, _Summary) ->
     Acc;
 
-index(Otp, [ DocSrc | Rest], Acc) ->
-    
-    NewAcc = case do_index(Otp, DocSrc) of
+index(Otp, [ DocSrc | Rest], Acc, Summary) ->
+    NewAcc = case do_index(Otp, DocSrc, Summary) of
                  ignore -> Acc;
                  Else   -> lists:append(Else, Acc)
              end,
-    
-    index(Otp, Rest, NewAcc).
+    index(Otp, Rest, NewAcc, Summary).
 
-do_index(Otp, Src) ->
+do_index(Otp, Src, Summary) ->
     
     {Type, _Attr, Content}
         = read_xml(root(), Otp, Src, [{space, normalize}]),
@@ -77,9 +98,12 @@ do_index(Otp, Src) ->
             % strip silly shy characters
             Mod  = [ X || X <- string:join(Module, ""), X =/= 173],
             Funs = get_funs(App, Mod, lists:keyfind(funcs, 1, Xml)),
-            Summary = string:substr(Sum, 1, 50),
+            Sum2 = case Summary of
+                       short -> string:substr(Sum, 1, 50);
+                       long  -> Sum
+                   end,
             
-            [ ["mod", App, Mod, Summary] |  Funs]    
+            [ ["mod", App, Mod, Sum2] |  Funs]    
     end.
 
 format(Str, Args) ->
@@ -214,34 +238,72 @@ find_docs(Otp) ->
     
     lists:foldl(fun lists:append/2, [], Docs) ++ Erts.
 
+info(Base) ->
+    [
+     {p, [], ["erldocs.com is an alternative to the official erlang documentation, "
+              "created by Dale Harvey. All content is &copy; Ericsson."]},
+     {p, [],
+      ["The source code can be found on ",
+       {a, [{href,"http://github.com/daleharvey/erldocs.com/tree/master"}],
+        ["Github"]},
+       ",  you can email me at ",
+       {a, [{href,"mailto:dale@arandomurl.com"}], ["dale@arandomurl.com"]},
+       " for bug reports / feature requests or just feedback :)"
+      ]},
+     {em, [], ["Alternative Erlang Versions"]},
+     {ul, [], [{li, [], [{a, [{href,"/otp_src_R13B/"}], ["R13B"]}]}]},
+     {br, [], []},
+     {em, [], ["Other Places:"]},
+     {ul, [],
+      [{li, [],
+        [{a, [{href,"http://www.erlang.org/doc/"}],
+          ["http://www.erlang.org/doc/"]},
+         {br, [], []},
+         "The official erlang documentation"]},
+       {li, [],
+        [{a, [{href,"http://learnyousomeerlang.com/"}],
+          ["http://learnyousomeerlang.com/"]},
+         {br, [], []},
+         "A beginners guide to erlang"]},
+       {li, [],
+        [{a, [{href,Base++"module_index.html"}], ["module_index.html"]},
+         {br, [], []},
+         "A static index of erlang modules"]}]}
+    ].
+
 % eugh: template to wrap pages in
 erlref_wrap(Module, Xml, Base) ->
-    [{html, [{lang, "en"}], ["\n",
-         {head, [], [
+    [
+     {html, [{lang, "en"}],
+      ["\n",
+       {head, [],
+        [
          {meta,  [{charset, "utf-8"}], []},"\n",
          {title, [], [Module ++ " - erldocs.com"]},"\n",
          {link,  [{type, "text/css"}, {rel, "stylesheet"},
                   {href, Base++"../erldocs.css"}], []}, "\n"
-       ]},
-       {body, [], [
-         {'div', [{id, "sidebar"}], [
+        ]},
+       {body, [],
+        [
+         {'div', [{id, "sidebar"}],
+          [
            {input, [{type, "text"}, {value,"Loading..."}, {id, "search"},
                     {autocomplete, "off"}], []},
-           {ul, [{id, "results"}], [" "]},
-	   {'div', [{class, "copystuff"}], [
-             {span, [], [" © Ericsson, "]},
-	     {a, [{target, "blank"}, {href, "http://github.com/daleharvey/erldocs.com/tree"}], ["Source (github)"]}
-           ]}		     
-         ]},
+           {a, [{id, "expand"}],["▾"]},
+           {'div', [{id, "sideinfo"}, {class, "info"}], info(Base)},
+           {ul, [{id, "results"}], [" "]}
+          ]},
          {'div', [{id, "content"}], Xml},
          {script, [{src, "http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js"}], [" "]},
          {script, [{src, Base++"erldocs_index.js"}], [" "]},
          {script, [{src, Base++"../erldocs.js"}], [" "]}
-       ]}
-     ]}].
+        ]}
+      ]}].
 
-index_tpl() ->
-    [{h2, [], ["Welome to erldocs.com"]}].
+index_tpl(Vsn, Base) ->
+    [{h1, [], ["Erlang "++Vsn++" Documentation"]},
+     {hr, [], []}, 
+     {'div', [{class, "info"}], info(Base)}].
 
 % Transforms erlang xml format to html
 tr_erlref({header,[],_Child}, _Acc) ->
@@ -276,6 +338,8 @@ tr_erlref({input, [], Child}, _Acc) ->
     {code, [], Child};
 tr_erlref({item, [], Child}, _Acc) ->
     {li, [], Child};
+tr_erlref({pre, [], Child}, _Acc) ->
+    {pre, [{class, "sh_erlang"}], Child};
 tr_erlref({list, _Type, Child}, _Acc) ->
     {ul, [], Child};
 tr_erlref({name, [], Child}, Acc) ->
